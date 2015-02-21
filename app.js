6ecdf485902dae48633686cce83b26cd272ecdc1
Colin Mackey <colin@fishbrains.com>
2015-02-21 15:06:32 -0500

    alert!

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var VAbusinessApp = angular.module('VAbusinessApp', [])
 VAbusinessApp.controller('messageController', ['$scope', function($scope) {
     $scope.keycounter = 0;
     $scope.change = function() {
+        alert("Change!!");
         $scope.counter++;
     }
 }]);
