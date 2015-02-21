3bd494f58f025f8cc182ffd7971d171877e82555
Colin Mackey <colin@fishbrains.com>
2015-02-21 15:08:18 -0500

    checking counter

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var VAbusinessApp = angular.module('VAbusinessApp', [])
 VAbusinessApp.controller('messageController', ['$scope', function($scope) {
     $scope.keycounter = 0;
     $scope.change = function() {
-        alert("Change!!");
         $scope.counter++;
     }
 }]);
