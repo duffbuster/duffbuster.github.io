c6249007cbf515ec82cab0965a9e2614a951846e
Colin Mackey <colin@fishbrains.com>
2016-11-08 22:11:39 -0500

    Changing to resume

--- app.js
+++
@@ -1,8 +0,0 @@
-var VAbusinessApp = angular.module('VAbusinessApp', [])
-
-VAbusinessApp.controller('messageController', ['$scope', function($scope) {
-    $scope.keycounter = 0;
-    $scope.change = function() {
-        $scope.keycounter++;
-    }
-}]);
